34f43517f3ed79eda355316b212f05cf425a543f
datzz7 <43978289+datzz7@users.noreply.github.com>
2019-10-24 15:40:02 +0800

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -39,7 +39,7 @@ require 'vendor/autoload.php';
 			    $mail->isSMTP();                                            // Send using SMTP
 			    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
 			    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-			    $mail->Username   = 'dats.astapan@gmail.com';                     // SMTP username
+			    $mail->Username   = 'bus.ticketing.no.reply@gmail.com';                     // SMTP username
 			    $mail->Password   = 'Datsjayson#7';                               // SMTP password
 			    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
 			    $mail->Port       = 587;                                    // TCP port to connect to
