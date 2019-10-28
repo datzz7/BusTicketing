e7abbcb1151c914a9f982ccd473f39d09f29eb9e
datzz7 <43978289+datzz7@users.noreply.github.com>
2019-10-28 21:50:34 +0800

    Update check_email.php

--- check_email.php
+++ check_email.php
@@ -1,6 +1,11 @@
 <?php 
 
-include 'conn.php';
+require 'conn.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
 
 $email = $_POST['email'];
 
@@ -10,28 +15,66 @@ $check = "SELECT * FROM users where email='$email'";
  $result = mysqli_fetch_assoc($response);
 
  
- // $result['exists'] = array();
- 
 
 if(!$result['email']==$email){
 
-	sleep(1);
-		$res['message'] ="0";
-		echo json_encode($res);
-		mysqli_close($conn);
+		$emailTo = $email;
+		$code = uniqid(true);
 
-}
+		$delete = "DELETE FROM temp_email_verification where email ='$emailTo'";
+		$conn->query($delete);
+	 	
 
-else{		
+		$mail = new PHPMailer(true);
 
-	sleep(1);
+			try {
+				$sql = "INSERT INTO temp_email_verification VALUES('$emailTo', '$code')";
+				$conn->query($sql);
+			    //Server settings
+			    //$mail->SMTPDebug = 1;                     
+			    $mail->isSMTP();                                           
+			    $mail->Host       = 'smtp.gmail.com';                  
+			    $mail->SMTPAuth   = true;                                   
+			    $mail->Username   = 'bus.ticketing.no.reply@gmail.com';                 
+			    $mail->Password   = 'Datsjayson#7';                             
+			    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         
+			    $mail->Port       = 587;                                   
 
-	$res['message'] ="1";
-	echo json_encode($res);	
-	mysqli_close($conn);
-		
-	}
-	
+			    //Recipients
+			    $mail->setFrom('bus-ticketing-no-reply@gmail.com', 'Bus Ticketing');
+			    $mail->addAddress($emailTo);     // Add a recipient
+			   //$mail->addAddress('ellen@example.com');               // Name is optional
+			   //$mail->addReplyTo('info@example.com', 'Information');
+			   //$mail->addCC('cc@example.com');
+			   //$mail->addBCC('bcc@example.com');
+
+			    // Content
+			    $mail->isHTML(true);                                  // Set email format to HTML
+			    $mail->Subject = 'Verify Email';
+			    $mail->Body    = '<h1>This contains your email verification code</h1>
+										use this code: '. $code. ' to verifiy and proceed to registration.';
+			    $mail->AltBody = '';							//This is the body in plain text for non-HTML mail clients
+
+			    $mail->send();
+
+
+				$res['message'] ="0";
+				echo json_encode($res);
+				mysqli_close($conn);
+
+			}
+			catch (Exception $e) {
+					    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+					}
+		}
+
+		else{		
+
+			$res['message'] ="1";
+			echo json_encode($res);	
+			mysqli_close($conn);
+				
+			}
 
 
 
