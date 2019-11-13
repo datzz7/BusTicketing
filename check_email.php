d7ff8c6160f145d862c829d14221c2e4f5ff3663
datzz7 <43978289+datzz7@users.noreply.github.com>
2019-11-14 01:02:24 +0800

    Update check_email.php

--- check_email.php
+++ check_email.php
@@ -27,7 +27,7 @@ if(!$result['email']==$email){
 		$emailTo = $email;
 		
 		$path = "img/$email-$code.png";
-		$actualpath = "http://bus-ticketing.herokuapp.com/thesis/$path";
+		$actualpath = "http://bus-ticketing.herokuapp.com/$path";
 		$resimage = file_put_contents($path,base64_decode($image));
 
 		$delete = "DELETE FROM temp_email_verification where email ='$emailTo'";
