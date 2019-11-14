0aaf2d0b8bc9c7f381f7d30e8be1821ecb574d9a
datzz7 <43978289+datzz7@users.noreply.github.com>
2019-11-15 02:48:05 +0800

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -36,6 +36,7 @@ if(!isset($_SESSION['username']))
   <a href="total_highlight.php" class="w3-bar-item w3-button">Sales Reports</a>
   <a href="add_prices_highlight.php" class="w3-bar-item w3-button">Add prices</a>
   <a href="edit_prices_highlight.php" class="w3-bar-item w3-button">Edit Default Prices</a>
+  <a href="bus_route_highlight.php" class="w3-bar-item w3-button">Manage Route</a>
   <a href="logging_out.php" class="w3-bar-item w3-button">Log Out</a>
 </div>
 </form>
