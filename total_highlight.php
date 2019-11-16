b11ed281e10baa1ecb039550352a84055957ac67
datzz7 <43978289+datzz7@users.noreply.github.com>
2019-11-16 21:27:10 +0800

    Update total_highlight.php

--- total_highlight.php
+++ total_highlight.php
@@ -1,10 +1,10 @@
 <?php
   include "conn.php";
-  session_start();
-if(!isset($_SESSION['username']))
-  {
-   header('location: admin_login.php');
-  }
+//   session_start();
+// if(!isset($_SESSION['username']))
+//   {
+//    header('location: admin_login.php');
+//   }
 ?>
 <!doctype html>
 <html lang="en">
