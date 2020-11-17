de852319349083669a995960d6936885cec52d0e
datzz7 <dats.astapan@gmail.com>
2020-11-17 23:37:01 +0800

    changed to postgres

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php 
 
-$server="sql12.freemysqlhosting.net";
-$user="sql12309635";
-$password="RNJxAraLq9";
-$dbname="sql12309635";
+$server="ec2-35-168-77-215.compute-1.amazonaws.com";
+$user="unwnmcpugqljbn";
+$password="9814c64a8614bc5f2686ae74f9a1a0327d745c0f40fc4b93035932b8bdab95e5";
+$dbname="d80artqjpkfnbo";
 
 $conn = new mysqli($server, $user, $password, $dbname);
 
