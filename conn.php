8197092d719a6cd548e34d0ff620fa5eeb595f36
Dats <dats.astapan@gmail.com>
2020-11-18 01:24:39 +0800

    test postgres

--- conn.php
+++ conn.php
@@ -1,16 +1,14 @@
 <?php 
 
-$server="ec2-35-168-77-215.compute-1.amazonaws.com";
-$user="unwnmcpugqljbn";
-$password="9814c64a8614bc5f2686ae74f9a1a0327d745c0f40fc4b93035932b8bdab95e5";
-$dbname="d80artqjpkfnbo";
 
-$conn = new mysqli($server, $user, $password, $dbname);
+$conn_string = "host=ec2-35-168-77-215.compute-1.amazonaws.com port=5432 dbname=d80artqjpkfnbo user=unwnmcpugqljbn password=
+9814c64a8614bc5f2686ae74f9a1a0327d745c0f40fc4b93035932b8bdab95e5";
+$dbconn = pg_connect($conn_string);
 
 // Check Connection
-if ($conn->connect_error)
+if ($dbconn->connect_error)
 {
-	die("Connection failed: ". $conn->connect_error);
+	die("Connection failed: ". $dbconn->connect_error);
 
 }
 echo "Connected";
