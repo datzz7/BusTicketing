2cd11de13a40b889570f496e9afcb21041db701f
datzz7 <dats.astapan@gmail.com>
2020-11-18 00:58:45 +0800

    test

--- conn.php
+++ conn.php
@@ -1,11 +1,11 @@
 <?php 
 
-$server="ec2-35-168-77-215.compute-1.amazonaws.com";
-$user="unwnmcpugqljbn";
-$password="9814c64a8614bc5f2686ae74f9a1a0327d745c0f40fc4b93035932b8bdab95e5";
-$dbname="d80artqjpkfnbo";
+// $server="ec2-35-168-77-215.compute-1.amazonaws.com";
+// $user="unwnmcpugqljbn";
+// $password="9814c64a8614bc5f2686ae74f9a1a0327d745c0f40fc4b93035932b8bdab95e5";
+// $dbname="d80artqjpkfnbo";
 
-$conn = new mysqli($server, $user, $password, $dbname);
+// $conn = new mysqli($server, $user, $password, $dbname);
 
 // Check Connection
 // if ($conn->connect_error)
@@ -13,5 +13,5 @@ $conn = new mysqli($server, $user, $password, $dbname);
 // 	die("Connection failed: ". $conn->connect_error);
 
 // }
-// echo "Connected";
+ echo "Connected";
 ?>
