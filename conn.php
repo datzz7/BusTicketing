dc1c27a4aad29f8f2eeb5d40f359b50400f14673
clyde migue <clyde.migue@gmail.com>
2020-11-18 10:46:37 +0900

    updated conn.php

--- conn.php
+++ conn.php
@@ -1,9 +1,19 @@
 <?php 
 
+$host = "ec2-35-168-77-215.compute-1.amazonaws.com";
+$db = "d80artqjpkfnbo";
+$username = "unwnmcpugqljbn";
+$password = "9814c64a8614bc5f2686ae74f9a1a0327d745c0f40fc4b93035932b8bdab95e5";
 
-$conn_string = "host=ec2-35-168-77-215.compute-1.amazonaws.com port=5432 dbname=d80artqjpkfnbo user=unwnmcpugqljbn password=
-9814c64a8614bc5f2686ae74f9a1a0327d745c0f40fc4b93035932b8bdab95e5";
-$dbconn = pg_connect($conn_string);
+$dbconn = pg_connect("host=$this->host dbname=$this->db user=$this->username password=$this->password");
+
+// $conn_string = "
+// host=ec2-35-168-77-215.compute-1.amazonaws.com 
+// port=5432 
+// dbname=d80artqjpkfnbo 
+// user=unwnmcpugqljbn 
+// password=9814c64a8614bc5f2686ae74f9a1a0327d745c0f40fc4b93035932b8bdab95e5";
+// $dbconn = pg_connect($conn_string);
 
 // Check Connection
 
